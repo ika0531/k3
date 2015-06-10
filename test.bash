e0403ac4c59f5a7ba3918cb5a840dcf23f957f37
Kazuhiro Iwatsubo <a1508ki@aiit.ac.jp>
2015-06-10 21:52:20 +0900

    Add Test

---
+++ test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+error () {
+	echo "$1" >&2
+	exit 1
+}
+
+# 値が正しいか確認
+[ $(./15745108 20 12) -eq 4 ] || error "ERROR1" 
+[ $(./15745108 30 12) -eq 6 ] || error "ERROR2" 
+[ $(./15745108 45 12) -eq 3 ] || error "ERROR3" 
+
+# 終了ステータスが0で終わるか確認
+./15745108 45 12 > /dev/null || error "ERROR4"
+
+echo OK
+
+exit 0
